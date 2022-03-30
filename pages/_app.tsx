95dccce6ef47f629f449e5cac0a49f93cd332c0e
Sharad Chand <pepsighan@sharadchand.com>
2022-03-30 13:35:09 +0530

    Add seo for the app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,22 @@
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import theme from 'config/theme';
+import { NextSeo } from 'next-seo';
 import type { AppProps } from 'next/app';
-import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <NextSeo
+        title="Athanni Finance"
+        description="A fast and cheap non-custodial token swap on Theta Network."
+        additionalMetaTags={[
+          { name: 'viewport', content: 'initial-scale=1, width=device-width' },
+        ]}
+      />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
