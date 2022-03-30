34ec136980576e1a9fb0a3dde01a5323a829b45d
Sharad Chand <pepsighan@sharadchand.com>
2022-03-30 15:51:08 +0530

    Properly center the navigation button

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,9 +1,11 @@
 import {
   AppBar,
   Avatar,
+  Box,
   Button,
   ButtonGroup,
   IconButton,
+  Stack,
   Toolbar,
 } from '@mui/material';
 import Link from 'next/link';
@@ -15,30 +17,36 @@ export default function Navigation() {
   return (
     <AppBar color="transparent" position="sticky" elevation={0}>
       <Toolbar sx={{ justifyContent: 'space-between' }}>
-        <IconButton>
-          <Avatar>A</Avatar>
-        </IconButton>
+        <Box flex={1}>
+          <IconButton>
+            <Avatar>A</Avatar>
+          </IconButton>
+        </Box>
 
-        <ButtonGroup variant="contained">
-          <Link href="/" passHref>
-            <Button
-              component="a"
-              color={pathname === '/' ? 'secondary' : 'inherit'}
-            >
-              Swap
-            </Button>
-          </Link>
-          <Link href="/pool" passHref>
-            <Button
-              component="a"
-              color={pathname === '/pool' ? 'secondary' : 'inherit'}
-            >
-              Pool
-            </Button>
-          </Link>
-        </ButtonGroup>
+        <Stack flex={1} alignItems="center">
+          <ButtonGroup variant="contained">
+            <Link href="/" passHref>
+              <Button
+                component="a"
+                color={pathname === '/' ? 'secondary' : 'inherit'}
+              >
+                Swap
+              </Button>
+            </Link>
+            <Link href="/pool" passHref>
+              <Button
+                component="a"
+                color={pathname === '/pool' ? 'secondary' : 'inherit'}
+              >
+                Pool
+              </Button>
+            </Link>
+          </ButtonGroup>
+        </Stack>
 
-        <Button variant="contained">Connect Wallet</Button>
+        <Stack alignItems="flex-end" flex={1}>
+          <Button variant="contained">Connect Wallet</Button>
+        </Stack>
       </Toolbar>
     </AppBar>
   );
