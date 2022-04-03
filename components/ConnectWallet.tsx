50bde7d30cc6781c910e497fdcaabc77eb7d8ef7
Sharad Chand <pepsighan@sharadchand.com>
2022-04-03 09:32:41 +0530

    Show address even if wallet not connected to proper chain id

--- components/ConnectWallet.tsx
+++ components/ConnectWallet.tsx
@@ -9,14 +9,14 @@ import {
   ListItemIcon,
   ListItemText,
 } from '@mui/material';
-import { useWeb3React } from '@web3-react/core';
+import { useWeb3React, UnsupportedChainIdError } from '@web3-react/core';
 import { InjectedConnector } from '@web3-react/injected-connector';
 import {
   THETA_MAINNET_CHAIN_ID,
   THETA_TESTNET_CHAIN_ID,
 } from 'config/constants';
 import { useCallback } from 'react';
-import { useBoolean } from 'react-use';
+import { useAsync, useBoolean } from 'react-use';
 import { shorternAddress } from 'utils/string';
 import MetamaskIcon from './MetamaskIcon';
 
@@ -30,24 +30,37 @@ const injectedWallet = new InjectedConnector({
 
 export default function ConnectWallet({ buttonProps }: ConnectWalletProps) {
   const [open, toggleOpen] = useBoolean(false);
-  const { active, activate, chainId, account } = useWeb3React();
+  const { active, activate, account, error } = useWeb3React();
 
   const onConnectMetaMask = useCallback(async () => {
     await activate(injectedWallet);
     toggleOpen(false);
   }, [activate, toggleOpen]);
 
+  const { value: accountIfError } = useAsync(
+    async () =>
+      error instanceof UnsupportedChainIdError
+        ? injectedWallet.getAccount()
+        : null,
+    [error]
+  );
+
+  // It is connected even if wallet not on valid chain id.
+  const isConnected = active || error instanceof UnsupportedChainIdError;
+
   return (
     <>
       <Button variant="contained" {...buttonProps} onClick={toggleOpen}>
-        {active && chainId && account
-          ? shorternAddress(account)
-          : 'Connect Wallet'}
+        {active && account && shorternAddress(account)}
+        {error instanceof UnsupportedChainIdError &&
+          accountIfError &&
+          shorternAddress(accountIfError)}
+        {!account && !accountIfError && 'Connect Wallet'}
       </Button>
 
       {/* Show the dialog to connect wallet if its not connected. */}
       <Dialog
-        open={!active && open}
+        open={!isConnected && open}
         onClose={toggleOpen}
         fullWidth
         maxWidth="xs"
--- components/Swapper.tsx
+++ components/Swapper.tsx
@@ -1,16 +1,18 @@
-import { Button, Paper, TextField, Typography } from '@mui/material';
-import { useWeb3React } from '@web3-react/core';
+import { Paper, TextField, Typography } from '@mui/material';
+import { UnsupportedChainIdError, useWeb3React } from '@web3-react/core';
 import ConnectWallet from './ConnectWallet';
 
 export default function Swapper() {
-  const { active, chainId, account } = useWeb3React();
+  const { active, error } = useWeb3React();
+  // It is connected even if wallet not on valid chain id.
+  const isConnected = active || error instanceof UnsupportedChainIdError;
 
   return (
     <Paper sx={{ p: 2, width: '100%', maxWidth: 500 }}>
       <Typography variant="h6">Swap</Typography>
       <TextField placeholder="0.0" fullWidth sx={{ mt: 1 }} />
       <TextField placeholder="0.0" fullWidth sx={{ mt: 1, mb: 1 }} />
-      {!(active && chainId && account) && (
+      {!isConnected && (
         <ConnectWallet
           buttonProps={{
             size: 'large',
