558a9b551a57a3d97387bcddd6b85d29f1fcf919
Sharad Chand <pepsighan@sharadchand.com>
2022-04-03 20:31:40 +0530

    Added func to get factory contract

--- utils/ethers.ts
+++ utils/ethers.ts
@@ -2,12 +2,18 @@ import { useWeb3React } from '@web3-react/core';
 import config from 'config/config';
 import { ethers } from 'ethers';
 import { useMemo } from 'react';
+import { useAsync } from 'react-use';
+import factoryContractAbi from './factoryContractAbi';
 import routerContractAbi from './routerContractAbi';
+
+type RouterContract = ethers.Contract & {
+  factory(): Promise<string>;
+};
 
 /**
  * Gets the router contract API using ethers.
  */
-export function useRouterContract(): ethers.Contract | null {
+export function useRouterContract(): RouterContract | null {
   const { library } = useWeb3React();
   return useMemo(() => {
     if (!library) {
@@ -19,6 +25,33 @@ export function useRouterContract(): ethers.Contract | null {
       config.ROUTER_CONTRACT_ADDRESS,
       routerContractAbi,
       signer
-    );
+    ) as RouterContract;
   }, [library]);
+}
+
+/**
+ * Gets the factory contract API using ethers.
+ */
+export function useFactoryContract(): ethers.Contract | null {
+  const { library } = useWeb3React();
+  const routerContract = useRouterContract();
+
+  // The factory address is provided by the router itself. No need
+  // to hardcode that.
+  const { value: factoryAddress } = useAsync(async () => {
+    if (!routerContract) {
+      return null;
+    }
+
+    return await routerContract.factory();
+  }, [routerContract]);
+
+  return useMemo(() => {
+    if (!library || !factoryAddress) {
+      return null;
+    }
+
+    const signer = library.getSigner();
+    return new ethers.Contract(factoryAddress, factoryContractAbi, signer);
+  }, [factoryAddress, library]);
 }
