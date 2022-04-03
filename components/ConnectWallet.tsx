8de237e60ddbe40e2d7c45f9a418a98141c06842
Sharad Chand <pepsighan@sharadchand.com>
2022-04-03 08:55:17 +0530

    Implement connecting to metamask

--- components/ConnectWallet.tsx
+++ components/ConnectWallet.tsx
@@ -9,6 +9,13 @@ import {
   ListItemIcon,
   ListItemText,
 } from '@mui/material';
+import { useWeb3React } from '@web3-react/core';
+import { InjectedConnector } from '@web3-react/injected-connector';
+import {
+  THETA_MAINNET_CHAIN_ID,
+  THETA_TESTNET_CHAIN_ID,
+} from 'config/constants';
+import { useCallback } from 'react';
 import { useBoolean } from 'react-use';
 import MetamaskIcon from './MetamaskIcon';
 
@@ -16,8 +23,18 @@ type ConnectWalletProps = {
   buttonProps?: ButtonProps;
 };
 
+const injectedWallet = new InjectedConnector({
+  supportedChainIds: [THETA_TESTNET_CHAIN_ID, THETA_MAINNET_CHAIN_ID],
+});
+
 export default function ConnectWallet({ buttonProps }: ConnectWalletProps) {
   const [open, toggleOpen] = useBoolean(false);
+  const { active, activate } = useWeb3React();
+
+  const onConnectMetaMask = useCallback(async () => {
+    await activate(injectedWallet);
+    toggleOpen();
+  }, [activate, toggleOpen]);
 
   return (
     <>
@@ -25,11 +42,16 @@ export default function ConnectWallet({ buttonProps }: ConnectWalletProps) {
         Connect Wallet
       </Button>
 
-      <Dialog open={open} onClose={toggleOpen} fullWidth maxWidth="xs">
+      <Dialog
+        open={!active && open}
+        onClose={toggleOpen}
+        fullWidth
+        maxWidth="xs"
+      >
         <DialogTitle>Connect Wallet</DialogTitle>
         <DialogContent>
           <List>
-            <ListItem button>
+            <ListItem button onClick={onConnectMetaMask}>
               <ListItemIcon>
                 <MetamaskIcon />
               </ListItemIcon>
