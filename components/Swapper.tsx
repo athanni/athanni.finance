f893a9aae022e7bcf0d392b36e7c2468825e84cf
Sharad Chand <pepsighan@sharadchand.com>
2022-04-03 09:16:05 +0530

    Do not show connect button on swapper once connected

--- components/Swapper.tsx
+++ components/Swapper.tsx
@@ -1,18 +1,23 @@
 import { Button, Paper, TextField, Typography } from '@mui/material';
+import { useWeb3React } from '@web3-react/core';
 import ConnectWallet from './ConnectWallet';
 
 export default function Swapper() {
+  const { active, chainId, account } = useWeb3React();
+
   return (
     <Paper sx={{ p: 2, width: '100%', maxWidth: 500 }}>
       <Typography variant="h6">Swap</Typography>
       <TextField placeholder="0.0" fullWidth sx={{ mt: 1 }} />
       <TextField placeholder="0.0" fullWidth sx={{ mt: 1, mb: 1 }} />
-      <ConnectWallet
-        buttonProps={{
-          size: 'large',
-          fullWidth: true,
-        }}
-      />
+      {!(active && chainId && account) && (
+        <ConnectWallet
+          buttonProps={{
+            size: 'large',
+            fullWidth: true,
+          }}
+        />
+      )}
     </Paper>
   );
 }
