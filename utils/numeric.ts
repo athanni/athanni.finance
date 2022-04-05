aae3f5f4f3788b55a8c1e0f9acd54999a7c26389
Sharad Chand <pepsighan@sharadchand.com>
2022-04-05 16:55:50 +0530

    Only 1 dot allowed

--- utils/numeric.ts
+++ utils/numeric.ts
@@ -1,3 +1,4 @@
+import BigNumber from 'bignumber.js';
 import { ChangeEvent } from 'react';
 
 /**
@@ -7,20 +8,30 @@ export function handleDecimalInput(
   e: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>,
   onChange: (...values: any[]) => void
 ) {
-  const num = parseFloat(e.target.value);
   const hasSucceedingDot = e.target.value.endsWith('.');
+  // The user may try to add one more dot after a dot. Ignores the second dot and protects
+  // the input from being reset to 0.
+  const hasSucceedingDoubleDot = e.target.value.endsWith('..');
+  // To check if a dot is already within the number so there can be no second dot.
   const containsDotBefore = e.target.value
-    .substring(0, e.target.value.length - 1)
+    .substring(0, e.target.value.length - (hasSucceedingDoubleDot ? 2 : 1))
     .includes('.');
 
-  if (Number.isNaN(num)) {
+  // Prune any dots from the end so that number does not fail to parse and reset to 0.
+  let value = e.target.value;
+  if (hasSucceedingDot || hasSucceedingDoubleDot) {
+    value = value.replace(/\.+$/, '');
+  }
+  const num = new BigNumber(value);
+
+  if (num.isNaN()) {
     return onChange('0');
   }
 
   // Only allow one decimal point within the input.
-  if (!containsDotBefore && hasSucceedingDot) {
-    return onChange(`${num}.`);
+  if (!containsDotBefore && (hasSucceedingDot || hasSucceedingDoubleDot)) {
+    return onChange(`${num.toString()}.`);
   }
 
-  return onChange(`${num}`);
+  return onChange(num.toString());
 }
