30fe9fd806473687b74c45ffb831ccb926993531
Sharad Chand <pepsighan@sharadchand.com>
2022-04-06 08:43:38 +0530

    Validate the slippage rate

--- utils/slippage.ts
+++ utils/slippage.ts
@@ -10,6 +10,10 @@ export function calculateSlippageMin(
   price: ethers.BigNumber,
   slippageRate: number
 ): ethers.BigNumber {
+  if (slippageRate < 0 || slippageRate > 1) {
+    throw new Error('Invalid slippage rate');
+  }
+
   const p = new BigNumber(price.toString());
   const slipped = p.multipliedBy(new BigNumber(1).minus(slippageRate));
   return ethers.BigNumber.from(slipped.toFixed());
