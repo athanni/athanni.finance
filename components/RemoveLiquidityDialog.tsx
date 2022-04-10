dc05cc1b342b4c0dca5bead3d18847a9ac897813
Sharad Chand <pepsighan@sharadchand.com>
2022-04-10 18:59:20 +0530

    Add form input for the dialog

--- components/RemoveLiquidityDialog.tsx
+++ components/RemoveLiquidityDialog.tsx
@@ -1,4 +1,12 @@
-import { Dialog, DialogTitle } from '@mui/material';
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  Slider,
+  Stack,
+  Typography,
+} from '@mui/material';
 import { tokenMap } from 'config/supportedTokens';
 
 type RemoveLiquidityDialogProps = {
@@ -18,10 +26,23 @@ export default function RemoveLiquidityDialog({
   const token1 = tokenMap[tokenB];
 
   return (
-    <Dialog open={open} onClose={onClose}>
+    <Dialog open={open} onClose={onClose} fullWidth maxWidth="xs">
       <DialogTitle>
         Remove Liquidity ({token0.ticker}-{token1.ticker})
       </DialogTitle>
+      <DialogContent>
+        <Stack spacing={2}>
+          <Typography fontWeight="medium">Amount</Typography>
+          <Stack direction="row" spacing={2} alignItems="center">
+            <Typography>0%</Typography>
+            <Slider valueLabelDisplay="auto" />
+            <Typography>100%</Typography>
+          </Stack>
+          <Button variant="contained" size="large" fullWidth>
+            Remove Liquidity
+          </Button>
+        </Stack>
+      </DialogContent>
     </Dialog>
   );
 }
