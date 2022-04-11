e7552f57530832ab631a5cb30911af173453a19b
Sharad Chand <pepsighan@sharadchand.com>
2022-04-11 14:17:46 +0530

    Switch the places for the tokens

--- components/Swapper.tsx
+++ components/Swapper.tsx
@@ -1,6 +1,9 @@
-import { Button, Paper, Stack, Typography } from '@mui/material';
+import { Button, IconButton, Paper, Stack, Typography } from '@mui/material';
+import { Box } from '@mui/system';
 import { UnsupportedChainIdError, useWeb3React } from '@web3-react/core';
+import { useCallback } from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
+import { MdSwapVert } from 'react-icons/md';
 import ConnectWallet from './ConnectWallet';
 import SwapInput from './SwapInput';
 
@@ -18,6 +21,15 @@ export default function Swapper() {
     },
   });
 
+  const { getValues, setValue } = form;
+  const onSwitchInput = useCallback(() => {
+    const fields = getValues();
+    setValue('tokenAAmount', fields.tokenBAmount);
+    setValue('tokenA', fields.tokenB);
+    setValue('tokenBAmount', fields.tokenAAmount);
+    setValue('tokenB', fields.tokenA);
+  }, [getValues, setValue]);
+
   return (
     <Paper
       variant="outlined"
@@ -30,21 +42,28 @@ export default function Swapper() {
     >
       <Typography fontWeight="medium">Swap</Typography>
       <FormProvider {...form}>
-        <Stack mt={3} spacing={3}>
+        <Stack mt={3}>
           <SwapInput isTokenA />
+          <Stack alignItems="center" my={1}>
+            <IconButton sx={{ bgcolor: 'grey.200' }} onClick={onSwitchInput}>
+              <MdSwapVert />
+            </IconButton>
+          </Stack>
           <SwapInput isTokenA={false} />
-          {isConnected ? (
-            <Button variant="contained" fullWidth size="large">
-              Swap
-            </Button>
-          ) : (
-            <ConnectWallet
-              buttonProps={{
-                size: 'large',
-                fullWidth: true,
-              }}
-            />
-          )}
+          <Box mt={3}>
+            {isConnected ? (
+              <Button variant="contained" fullWidth size="large">
+                Swap
+              </Button>
+            ) : (
+              <ConnectWallet
+                buttonProps={{
+                  size: 'large',
+                  fullWidth: true,
+                }}
+              />
+            )}
+          </Box>
         </Stack>
       </FormProvider>
     </Paper>
