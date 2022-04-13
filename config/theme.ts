a3584f59f0ee8883e48d4b5cdf04b417e51e094a
Sharad Chand <pepsighan@sharadchand.com>
2022-04-13 13:34:32 +0530

    Use inter font

--- config/theme.ts
+++ config/theme.ts
@@ -1,12 +1,18 @@
 import { createTheme } from '@mui/material';
 import { indigo } from '@mui/material/colors';
 
+const fontFamily = 'Inter, "Helvetica", "Arial", sans-serif';
+
 export default createTheme({
   palette: {
     primary: indigo,
     // TODO: The secondary color is lighter than the primary even if same color.
     // IDK why?
     secondary: indigo,
+  },
+  typography: {
+    fontFamily,
+    allVariants: { fontFamily },
   },
   components: {
     MuiButtonBase: {
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,9 +4,15 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700&display=swap"
           rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
         />
       </Head>
       <body>
