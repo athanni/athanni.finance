98ac3dbfd8412456168da9d82b1689987c7f6e28
Sharad Chand <pepsighan@sharadchand.com>
2022-04-17 15:49:05 +0530

    Wrap the tx buttons of bridge with connect wrapper

--- components/BridgeDeposit.tsx
+++ components/BridgeDeposit.tsx
@@ -1,14 +1,17 @@
 import { Button } from '@mui/material';
 import BridgeInput from './BridgeInput';
+import ConnectWrapper from './ConnectWrapper';
 
 export default function BridgeDeposit() {
   return (
     <>
       <BridgeInput network="Rinkeby" />
       <BridgeInput network="Theta Testnet" />
-      <Button variant="contained" size="large">
-        Deposit
-      </Button>
+      <ConnectWrapper>
+        <Button variant="contained" size="large">
+          Deposit
+        </Button>
+      </ConnectWrapper>
     </>
   );
 }
--- components/BridgeWithdraw.tsx
+++ components/BridgeWithdraw.tsx
@@ -1,14 +1,17 @@
 import { Button } from '@mui/material';
 import BridgeInput from './BridgeInput';
+import ConnectWrapper from './ConnectWrapper';
 
 export default function BridgeWithdraw() {
   return (
     <>
       <BridgeInput network="Theta Testnet" />
       <BridgeInput network="Rinkeby" />
-      <Button variant="contained" size="large">
-        Withdraw
-      </Button>
+      <ConnectWrapper>
+        <Button variant="contained" size="large">
+          Withdraw
+        </Button>
+      </ConnectWrapper>
     </>
   );
 }
