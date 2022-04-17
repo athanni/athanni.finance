2a735874a497c3875b71d6324e1e8b739d2fdb5b
Sharad Chand <pepsighan@sharadchand.com>
2022-04-17 15:40:29 +0530

    Show the actual network of the input

--- components/BridgeWithdraw.tsx
+++ components/BridgeWithdraw.tsx
@@ -4,8 +4,8 @@ import BridgeInput from './BridgeInput';
 export default function BridgeWithdraw() {
   return (
     <>
-      <BridgeInput />
-      <BridgeInput />
+      <BridgeInput network="Theta Testnet" />
+      <BridgeInput network="Rinkeby" />
       <Button variant="contained" size="large">
         Withdraw
       </Button>
