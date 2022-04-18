60324657db270dabe92a6698c726e1453e067c8d
Sharad Chand <pepsighan@sharadchand.com>
2022-04-18 16:50:20 +0530

    Inject the form

--- components/BridgeDeposit.tsx
+++ components/BridgeDeposit.tsx
@@ -1,19 +1,29 @@
 import { Button, Stack } from '@mui/material';
 import rinkebyTokens from 'config/rinkebyTokens.json';
+import { FormProvider, useForm } from 'react-hook-form';
 import BridgeInput from './BridgeInput';
 import BridgeInputReadonly from './BridgeInputReadonly';
 import ConnectWrapper from './ConnectWrapper';
 
 export default function BridgeDeposit() {
+  const form = useForm({
+    defaultValues: {
+      address: '0x',
+      amount: '',
+    },
+  });
+
   return (
-    <Stack component="form" spacing={2}>
-      <BridgeInput network="Rinkeby" options={rinkebyTokens} />
-      <BridgeInputReadonly network="Theta Testnet" />
-      <ConnectWrapper>
-        <Button variant="contained" size="large">
-          Deposit
-        </Button>
-      </ConnectWrapper>
-    </Stack>
+    <FormProvider {...form}>
+      <Stack component="form" spacing={2}>
+        <BridgeInput network="Rinkeby" options={rinkebyTokens} />
+        <BridgeInputReadonly network="Theta Testnet" />
+        <ConnectWrapper>
+          <Button variant="contained" size="large">
+            Deposit
+          </Button>
+        </ConnectWrapper>
+      </Stack>
+    </FormProvider>
   );
 }
--- components/BridgeWithdraw.tsx
+++ components/BridgeWithdraw.tsx
@@ -1,19 +1,29 @@
 import { Button, Stack } from '@mui/material';
 import rinkebyTokens from 'config/rinkebyTokens.json';
+import { FormProvider, useForm } from 'react-hook-form';
 import BridgeInput from './BridgeInput';
 import BridgeInputReadonly from './BridgeInputReadonly';
 import ConnectWrapper from './ConnectWrapper';
 
 export default function BridgeWithdraw() {
+  const form = useForm({
+    defaultValues: {
+      address: '0x',
+      amount: '',
+    },
+  });
+
   return (
-    <Stack component="form" spacing={2}>
-      <BridgeInput network="Theta Testnet" options={rinkebyTokens} />
-      <BridgeInputReadonly network="Rinkeby" />
-      <ConnectWrapper>
-        <Button variant="contained" size="large">
-          Withdraw
-        </Button>
-      </ConnectWrapper>
-    </Stack>
+    <FormProvider {...form}>
+      <Stack component="form" spacing={2}>
+        <BridgeInput network="Theta Testnet" options={rinkebyTokens} />
+        <BridgeInputReadonly network="Rinkeby" />
+        <ConnectWrapper>
+          <Button variant="contained" size="large">
+            Withdraw
+          </Button>
+        </ConnectWrapper>
+      </Stack>
+    </FormProvider>
   );
 }
