8658ff587f1466b314c420c4f3bb1afc5ce06362
Sharad Chand <pepsighan@sharadchand.com>
2022-04-18 13:58:48 +0530

    Show info about the faucet

--- pages/bridge/deposit.tsx
+++ pages/bridge/deposit.tsx
@@ -1,7 +1,8 @@
-import { Container, Stack } from '@mui/material';
+import { Alert, Container, Stack, Link } from '@mui/material';
 import BridgeDeposit from 'components/BridgeDeposit';
 import Bridger from 'components/Bridger';
 import Navigation from 'components/Navigation';
+import NLink from 'next/link';
 
 export default function Deposit() {
   return (
@@ -12,6 +13,20 @@ export default function Deposit() {
           <Bridger>
             <BridgeDeposit />
           </Bridger>
+          <Alert
+            severity="info"
+            sx={{
+              mt: 2,
+              width: '100%',
+              maxWidth: 500,
+            }}
+          >
+            If you do not have any test tokens, you can you{' '}
+            <NLink href="/faucet" passHref>
+              <Link>this faucet</Link>
+            </NLink>
+            .
+          </Alert>
         </Stack>
       </Container>
     </>
