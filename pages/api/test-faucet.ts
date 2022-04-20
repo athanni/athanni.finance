7063e594721e490c8732597b9c3ac738accdebc8
Sharad Chand <pepsighan@sharadchand.com>
2022-04-20 13:06:29 +0530

    Validate the existence of token properly

--- pages/api/test-faucet.ts
+++ pages/api/test-faucet.ts
@@ -1,13 +1,9 @@
 import atnt20Abi from 'abi/ATNT20.json';
 import { RINKEBY_CHAIN_RPC_URL } from 'config/constants';
-import { rinkebyTokens } from 'config/supportedTokens';
+import { Network, resolveToken } from 'config/supportedTokens';
 import { ethers } from 'ethers';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { isValidAddress } from 'utils/address';
-
-// These are the various ERC20 tokens that the faucet supports. These
-// are dummy tokens counterparts of WETH, USDT, WBTC, etc on Rinkeby.
-const supportedTokens = rinkebyTokens.map((it) => it.address);
 
 // The private key that is the owner of the faucet. This environment is only
 // accessible in the backend.
@@ -31,7 +27,7 @@ export default async function handler(
   if (
     typeof address !== 'string' ||
     typeof token !== 'string' ||
-    !supportedTokens.includes(token) ||
+    resolveToken(token)?.network !== Network.Rinkeby ||
     !isValidAddress(address)
   ) {
     return res.status(400).json({ status: 400, message: 'Bad Request' });
