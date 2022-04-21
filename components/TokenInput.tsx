5e7389d360ccbba38ced11cf0df3de3db2750d30
Sharad Chand <pepsighan@sharadchand.com>
2022-04-21 12:55:54 +0530

    Implement adding tokens to the wallet

--- components/TokenInput.tsx
+++ components/TokenInput.tsx
@@ -5,15 +5,19 @@ import {
   Dialog,
   DialogContent,
   DialogTitle,
+  IconButton,
   List,
   ListItem,
   ListItemAvatar,
+  ListItemSecondaryAction,
   ListItemText,
+  Tooltip,
   Typography,
 } from '@mui/material';
+import { useWeb3React } from '@web3-react/core';
 import { resolveToken, Token } from 'config/supportedTokens';
-import { ReactNode, useMemo } from 'react';
-import { MdArrowDropDown } from 'react-icons/md';
+import { ReactNode, useCallback, useMemo } from 'react';
+import { MdAdd, MdArrowDropDown } from 'react-icons/md';
 import { useBoolean } from 'react-use';
 
 type TokenInputProps = {
@@ -80,38 +84,75 @@ export default function TokenInput({
         <DialogContent sx={{ p: 0 }}>
           <List>
             {tokens.map((tok) => (
-              <ListItem
+              <TokenInputOption
                 key={tok.address}
-                button
-                sx={{ px: 4 }}
+                token={tok}
                 onClick={() => {
                   if (onChange) {
                     onChange(tok.address);
                   }
                   toggleOpen(false);
                 }}
-              >
-                <ListItemAvatar>
-                  <Box
-                    component="img"
-                    src={tok.logoUrl}
-                    alt={tok.name}
-                    width={38}
-                    height={38}
-                    borderRadius="100%"
-                  />
-                </ListItemAvatar>
-                <ListItemText>
-                  <Typography variant="h6">{tok.ticker}</Typography>
-                  <Typography variant="body2" color="textSecondary">
-                    {tok.name}
-                  </Typography>
-                </ListItemText>
-              </ListItem>
+              />
             ))}
           </List>
         </DialogContent>
       </Dialog>
     </>
+  );
+}
+
+type TokenInputOptionProps = {
+  token: Token;
+  onClick?: () => void;
+};
+
+function TokenInputOption({ token, onClick }: TokenInputOptionProps) {
+  const { connector } = useWeb3React();
+  // Shows the token in the Metamask list.
+  const onAddToken = useCallback(async () => {
+    if (!connector || !token) {
+      return;
+    }
+
+    await (connector.provider as any).request({
+      method: 'wallet_watchAsset',
+      params: {
+        type: 'ERC20',
+        options: {
+          address: token.address,
+          symbol: token.ticker,
+          decimals: token.decimals,
+        },
+      },
+    });
+  }, [connector, token]);
+
+  return (
+    <ListItem button sx={{ px: 4 }} onClick={onClick}>
+      <ListItemAvatar>
+        <Box
+          component="img"
+          src={token.logoUrl}
+          alt={token.name}
+          width={38}
+          height={38}
+          borderRadius="100%"
+        />
+      </ListItemAvatar>
+      <ListItemText>
+        <Typography variant="h6">{token.ticker}</Typography>
+        <Typography variant="body2" color="textSecondary">
+          {token.name}
+        </Typography>
+      </ListItemText>
+      <ListItemSecondaryAction>
+        <Tooltip title="Add To MetaMask">
+          <IconButton size="small" onClick={onAddToken}>
+            <MdAdd />
+          </IconButton>
+        </Tooltip>
+      </ListItemSecondaryAction>
+    </ListItem>
   );
 }
